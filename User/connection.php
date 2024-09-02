8af58ec8bd037a2bf02a00e59488f3eaabf000f6
Gannah El-Fekky <gannaalaa02@gmail.com>
2024-09-03 02:23:30 +0300

    Update connection.php

--- User/connection.php
+++ User/connection.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "";
 $database ="case02";
 
-$connect = mysqli_connect($localhost, $username ,$password , $database);
+$connect = mysqli_connect($localhost, $username ,$password , $database); 
 if($connect){
     // echo"connected" ;
 }
@@ -18,6 +18,22 @@ if(isset($_POST['logout']))
     session_destroy();
     header("location:login.php");
 }
-    // error_reporting(0);
+    if(!empty($_SESSION['freelancer_id'])){
+        $id=$_SESSION['freelancer_id'];
+        $select="SELECT * FROM `freelancer` where `freelancer_id`=$id";
+        $run=mysqli_query($connect,$select);
+        $fetch=mysqli_fetch_assoc($run);
+        $ban=$fetch['ban'];
+        $date=$fetch['time'];
+        $old_date=strtotime("$date");
+        $current_date=strtotime("today");
+        if(($ban==1) && ($old_date <=$current_date)){
+            session_unset();
+            session_destroy();
+            header("location:login.php");
+        }
+    }
+
+    error_reporting(0);
 
 ?>
